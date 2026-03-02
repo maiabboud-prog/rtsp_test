e681791ec9d7ab87a014dc44d334c7fb08fa8e20
maiabboud-prog <mai.abboud@innosentra.com>
2026-03-02 12:51:42 +0000

    modify camera ip

--- rtsp.py
+++ rtsp.py
@@ -1,6 +1,6 @@
 import cv2
 
-rtsp_url = "rtsp://admin:1234567a@192.168.100.107:554/cam/realmonitor?channel=1&subtype=0"
+rtsp_url = "rtsp://admin:1234567a@192.168.0.67:554/cam/realmonitor?channel=1&subtype=0"
 
 cap = cv2.VideoCapture(rtsp_url)
 
@@ -18,7 +18,9 @@ while True:
 
     cv2.imshow("RTSP Stream", frame)
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    key = cv2.waitKey(1) & 0xFF
+    if key == ord('q') or key == 27:  # 27 is ESC
+        print("⏹ Exiting...")
         break
 
 cap.release()
